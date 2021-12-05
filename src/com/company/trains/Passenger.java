8afd29c9e2a6eb99b60ad74f8d3ace653eeea66f
Vl007 <skorikov777@yandex.ru>
2021-12-05 17:15:29 +0300

    07. Exceptions fix

--- src/com/company/trains/Passenger.java
+++ src/com/company/trains/Passenger.java
@@ -1,9 +1,14 @@
 package com.company.trains;
 
+import com.company.TransportationFormatException;
+
 public class Passenger extends Train {
 
     @Override
     public void transportation(Object cargo, String destination) {
+        if (!destination.matches("[a-zA-Zа-яА-Я _-]+")) {
+            throw new TransportationFormatException(destination);
+        }
         System.out.println(
                 useUpFuel(10) ?
                         String.format("Транспортируем груз %s в %s\n", cargo, destination)
